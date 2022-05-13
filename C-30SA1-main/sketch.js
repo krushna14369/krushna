825e426202a2882aeba326f3e0a2e609b246ecb1
Ambekar1 <86044168+Ambekar1@users.noreply.github.com>
2022-05-13 12:33:12 +0530

    Add files via upload

---
+++ C-30SA1-main/sketch.js
@@ -0,0 +1,78 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var fruit,rope;
+var fruit_con;
+
+var bg_img;
+var food;
+var rabbit;
+var bunny;
+
+function preload()
+{
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  rabbit = loadImage('Rabbit-01.png');
+}
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  button=createImg("cut_button.png")
+  button.position(220,30)
+  button.size(50,50);
+  button.mouseClicked(drop)
+  bunny=createSprite(250,600,20,20)
+  bunny.addImage(rabbit)
+  bunny.scale=0.2
+  ground = new Ground(200,680,600,20);
+
+  rope = new Rope(7,{x:245,y:30});
+  fruit = Bodies.circle(300,300,20);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con = new Link(rope,fruit);
+ 
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  imageMode(CENTER);
+  
+}
+
+function draw() 
+{
+  background(51);
+
+  image(bg_img,width/2,height/2,490,690);
+
+  image(food,fruit.position.x,fruit.position.y,70,70); 
+  
+
+  rope.show();
+  Engine.update(engine);
+  drawSprites();
+  ground.show();
+
+ 
+   
+}
+
+function drop(){
+  rope.break();
+  fruit_con.detach();
+  fruit_con=null;
+}
