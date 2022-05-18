724b1bf73beb1b2db4c804c0550ad5416d711475
Ambekar1 <86044168+Ambekar1@users.noreply.github.com>
2022-05-18 15:25:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,54 +7,163 @@ const Body = Matter.Body;
 const Composites = Matter.Composites;
 const Composite = Matter.Composite;
 
+
 let engine;
 let world;
-var ball;
-var blower;
-var blowerMouth;
-var button;
+var plank;
+var ground;
+var higherground;
+var con;
+var con2;
+var rope;
+var bubble,bubble_img;
+
+function preload()
+{
+  bubble_img = loadImage("bubble.png")
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  rabbit = loadImage('Rabbit-01.png');
+
+  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+  star_img = loadImage('star.png');
+  
+  blink.playing = true;
+  eat.playing = true;
+  sad.playing = true;
+  sad.looping= false;
+  eat.looping = false; 
+}
 
 function setup() {
-  var canvas = createCanvas(500, 500);
-
+  createCanvas(500,800);
+  frameRate(80);
   engine = Engine.create();
   world = engine.world;
 
-  ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80);
-  blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20);
-  blowerMouth = new BlowerMouth(width / 2 + 70, height / 2 + 20, 100, 90);
-  button = createButton("Click to Blow");
-  button.position(width / 2, height - 100);
-  button.class("blowButton");
-
-  button.mousePressed(blow);
-
-  //buttonPressed(blow);
+   var fruit_options = {
+    restitution: 0.8
+  }
   
-  //button = mousePressed(blow);
+  ground =new Ground(250,height-10,width,20);
+  fruit = Bodies.circle(100,400,15,fruit_options);
+  World.add(world,fruit);
   
-  //button.mousePressed();
+  bubble = createSprite(290,460,20,20);
+  bubble.addImage(bubble_img);
+  bubble.scale = 0.1;
+  
+  //bunny sprite
+  blink.frameDelay = 20;
+  eat.frameDelay = 20;
+  bunny = createSprite(270,100,100,100);
+  bunny.addImage(rabbit);
+  bunny.scale = 0.2;
+  higherground =new Ground(300,170,100,10);
 
+  bunny.addAnimation('blinking',blink);
+  bunny.addAnimation('eating',eat);
+  bunny.addAnimation('crying',sad);
+  bunny.changeAnimation('blinking');
+
+  rope = new Rope(4,{x:230,y:330});
+  rope2 = new Rope(4,{x:50,y:450});
+  con = new Link(rope,fruit);
+  con2 = new Link(rope2,fruit);
+
+  //btn 1
+  button = createImg('cut_btn.png');
+  button.position(200,320);
+  button.size(50,50);
+
+  button2 = createImg('cut_btn.png');
+  button2.position(30,420);
+  button2.size(50,50);
+  button2.mouseClicked(drop);
+  //button2.Clicked(drop);
+  
+  //button2.mousePress(drop);
+  
+  // button2.mouseClick(drop);
+
+ 
+
+  ellipseMode(RADIUS);
 }
 
-function draw() {
-  background(59);
+function draw() 
+{
+  background(51);
+  image(bg_img,0,0,width,height);
   Engine.update(engine);
+  
+  push();
+  imageMode(CENTER);
+  if(fruit!=null){
+    image(food,fruit.position.x,fruit.position.y,70,70);
+  }
+  pop();
 
-  blower.show();
-  ball.show();
-  blowerMouth.show();
+  ground.show();
+  higherground.show();
+  rope.show();
+  rope2.show();
+
+  if(collide(fruit,bunny,80)==true)
+  {
+   remove_rope();
+   bubble.visible = false;
+    World.remove(engine.world,fruit);
+    fruit = null;
+    //bunny.change('eating');
+
+    bunny.changeAnimation('eating');
+
+    //bunny.changeAnimation();
+
+    //bunny.Animation('eating');
+  }
+  
+  if(collide(fruit,bubble,40) == true)
+    {
+      engine.world.gravity.y = -1;
+      bubble.position.x = fruit.position.x;
+      bubble.position.y = fruit.position.y;
+    }
+
+  drawSprites();
+
 }
 
-function blow() {
+function drop()
+{
+  rope2.break();
+  con2.dettach();
+  con2 = null; 
+}
 
-  Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:-0.05});
+function remove_rope()
+{
+  rope.break();
+  con.dettach();
+  con = null; 
+}
 
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:0.05});
-  
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0.05, y:0.05});
-  
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:-0.05, y:0});
-
+function collide(body,sprite,x)
+{
+  if(body!=null)
+        {
+         var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
+          if(d<=x)
+            {
+              
+               return true; 
+            }
+            else{
+              return false;
+            }
+         }
 }
 
